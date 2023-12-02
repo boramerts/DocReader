e04aa1b3f8ff7992eef5491c0abd2dab9a5cc163
boramerts <mertbora723@gmail.com>
2023-12-02 14:43:02 +0300

    Initial Commit

---
+++ DocReader/DocReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocReaderApp.swift
+//  DocReader
+//
+//  Created by Bora Mert on 2.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct DocReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
